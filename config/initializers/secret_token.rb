5aa61c7fd0067cf46657b9db165ba2ed2f041f86
Daniel Sparano <dsparano1@gmail.com>
2013-10-16 15:34:53 -0400

    RutkinWolf-InitialUpload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RutkinWolf::Application.config.secret_token = 'a0002295b4569f028c962db39f1e0a4839cb212f56f18075ed9bee319873187890932e81bf9b9bdeb4efef4488daae1ecf063ccc9464899427b3a0d9319125c2'
